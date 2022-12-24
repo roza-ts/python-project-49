d6ee9f91bd4ff684d0a6258dd3cfabfa68b34ff3
RozaTsy <ayara2015@mail.ru>
2022-12-25 02:13:33 +0300

    update brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -8,15 +8,16 @@ def task():
 def result():
     """Returns tuple that contains random number and word "yes" if it's prime and word "not" if it's not"""
 
-    number = randint(-100, 100)
+    number = randint(1, 100)
     return number, 'yes' if is_prime(number) else 'no'
 
 
 def is_prime(number):
     """Reports if number is prime"""
-
-    end = abs(number)
-    for num in range(2, end):
+    
+    if number == 1:
+        return False
+    for num in range(2, number):
         if number % num == 0:
             return False
     return True
