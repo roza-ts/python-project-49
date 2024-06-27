8e325f6ff413538d87183a2f13004f8cba971732
roza-ts <ayara2015@mail.ru>
2024-06-27 19:29:41 +0300

    update brain_even.py

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,14 @@
+from random import choice
+
+
+def task():
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def game():
+    num = choice(range(0, 1001))  
+    return num, ('no', 'yes')[is_even(num)]
+
+
+def is_even(number):
+    return number % 2 == 0
