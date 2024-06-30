b6a7ae3a5a95e047c91a8da60b6476579a1393d6
roza-ts <ayara2015@mail.ru>
2024-07-01 00:33:17 +0300

    add scripts/brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import brain_games.games.brain_prime
+import brain_games.engine
+
+
+def main():
+	print(brain_games.engine.action(brain_games.games.brain_prime))
+
+
+if __name__ == '__main__':
+	main()
