51713611ebfb77c127af35f9f7bd784a190dae1d
roza-ts <ayara2015@mail.ru>
2024-07-01 20:39:54 +0300

    update scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 
-import brain_games.games.brain_even
+import brain_games.games.brain_even as brain_even
 from brain_games.engine import begin
 
 
 def main():
-    begin(brain_games.games.brain_even)
+    begin(brain_even)
 
 
 if __name__ == '__main__':
